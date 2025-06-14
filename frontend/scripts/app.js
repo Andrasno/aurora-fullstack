03f30acd44c3fe2ea86bcca5cdd72c75e75a4a2c
Andrasno <andrasno@gmail.com>
2025-06-14 11:12:15 -0300

    alterando js para prod

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -85,7 +85,7 @@ form.addEventListener('submit', async (event) => {
 
     try {
         // Faz a requisição para a API Flask
-        const response = await fetch('http://127.0.0.1:8000/analise_multimodal', {
+        const response = await fetch('https://aurora-api-4us7.onrender.com/analise_multimodal', {
             method: 'POST',
             body: formData,
         });
