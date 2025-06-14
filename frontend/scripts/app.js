4b064d510bcd63582983539676bfed790cbb5732
Andrasno <andrasno@gmail.com>
2025-06-14 16:03:53 -0300

    mais uma correcao no js

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -85,7 +85,7 @@ form.addEventListener('submit', async (event) => {
 
     try {
         // Faz a requisição para a API Flask
-        const response = await fetch('https://aurora-api-q3la.onrender.com', {
+        const response = await fetch('https://aurora-api-q3la.onrender.com/analise_texto', {
             method: 'POST',
             body: formData,
         });
